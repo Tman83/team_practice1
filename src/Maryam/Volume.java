1af5a55ff269414bda47cf4d53ffb5d6e7d0212d
mariajoana <gokalp.maryam@gmail.com>
2022-05-13 20:33:05 +0100

    Testing 1 commit

---
+++ src/Maryam/Volume.java
@@ -0,0 +1,22 @@
+package Maryam;
+
+public interface Volume {
+
+    boolean hasVolume= true;
+
+    double volume();
+
+   /*
+    default void drink(){
+         System.out.println("Drinks water");/
+     }
+   */
+
+}
+
+/*
+abstract class QQ{
+}
+interface R{
+}
+*/
